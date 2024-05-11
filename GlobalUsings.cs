5673efa330651803698d135e1fab093653484afe
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 13:28:17 +0200

    Removed usings outside of projects

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,17 +1,17 @@
-global using SunamoEnums.Enums;
+
 global using System.Collections.Generic;
 global using System;
 global using System.Linq;
 global using System.Text;
 global using System.Collections;
-global using SunamoExceptions.InSunamoIsDerivedFrom;
-global using SunamoExceptions.OnlyInSE;
-global using SunamoValues;
-global using SunamoExceptions;
-global using SunamoData.Data;
-global using SunamoPercentCalculator;
+
+
+
+
+
+
 global using System.Data;
-global using SunamoAttributes;
+
 
 global using System.Collections.Generic;
 global using System.Collections;
