586955c3182ae98e0982b744b0ecaa9f7b5d1c25
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-05 11:11:13 +0200

    24.10.5.1 Added method

--- CA.cs
+++ CA.cs
@@ -17,6 +17,30 @@ public partial class CA
         throw new NotImplementedException();
     }
 
+    public static List<List<T>> DivideBy<T>(List<T> divs, int countOfColumn)
+    {
+        if (divs.Count % countOfColumn != 0)
+        {
+            throw new Exception($"Elements in {nameof(divs)} is not dividable by {nameof(countOfColumn)}");
+        }
+
+        List<List<T>> result = new List<List<T>>();
+
+        List<T> t = new List<T>();
+
+        foreach (var item in divs)
+        {
+            t.Add(item);
+            if (t.Count == countOfColumn)
+            {
+                result.Add(t);
+                t = new List<T>();
+            }
+        }
+
+        return result;
+    }
+
     public static List<List<T>> DivideByPercent<T>(List<T> ls, int v)
     {
         var parts = 100 / v;
